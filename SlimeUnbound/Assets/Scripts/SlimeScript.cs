d3affb09ca2f23e8936b3c699bb83bf8fe42fdf1
QuantumChord <60354100+QuantumChord@users.noreply.github.com>
2022-09-13 10:29:53 -0400

    Working with Pickups and Such

--- SlimeUnbound/Assets/Scripts/SlimeScript.cs
+++ SlimeUnbound/Assets/Scripts/SlimeScript.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class SlimeScript : MonoBehaviour
 {
+    public int points;
+
     // Start is called before the first frame update
     void Start()
     {
+        points = 0;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Collectibles")
+		{
+            points++;
+            Destroy(other.gameObject);
+        }
         
     }
 
