d0e9b0f3cba0bba49b433150de42a5444203d355
QuantumChord <60354100+QuantumChord@users.noreply.github.com>
2022-11-16 22:54:59 -0500

    Basic Bat Enemy

--- SlimeUnbound/Assets/Scripts/BatEnemy.cs
+++ SlimeUnbound/Assets/Scripts/BatEnemy.cs
@@ -1,18 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BatEnemy : MonoBehaviour
 {
+    public Transform player;
+    public Transform bat;
+    protected NavMeshAgent batMesh;
+
+    public double distanceFromPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        batMesh = GetComponent<NavMeshAgent>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        distanceFromPlayer = Vector3.Distance(bat.position, player.position);
+
+        if(distanceFromPlayer > 40)
+		{
+            batMesh.ResetPath();
+		}
+
+		else
+		{
+            batMesh.SetDestination(player.position);
+            transform.LookAt(player);
+            transform.rotation *= Quaternion.FromToRotation(Vector3.left, Vector3.forward);
+		}
     }
 }
